17b8c0cd5ff0273bf104265ea19d572460e3b775
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2022-02-14 16:46:39 -0800

    PracticeLinq

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var gamesList = new List<string>() { "Mario", "Zelda", "FIFA" };
+            var orderList = gamesList.OrderByDescending(x => x.Length).ToList();
+            foreach(var games in orderList)
+            {
+                Console.WriteLine(games);
+            }
+        }
+
+       
+    }
+
+    
+}
